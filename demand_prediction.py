ff17dbf771f6b0e45b03df8fec7c69c8f709698c
Amelie-Mad <madrona.amelie@gmail.com>
2025-05-06 15:29:24 +0200

    creating a problem in 1 day and 1 center (suproblem)

--- demand_prediction.py
+++ demand_prediction.py
@@ -9,34 +9,6 @@ dist, time = utils.get_distance(bogota, medellin)
 print(f"Distance: {dist} km")
 print(f"Time: {time} h")
 
-data1 = pd.read_excel('Clientes Colombia Marzo 2.xlsx')
-data2 = pd.read_excel('Clientes Colombia Marzo 3 (1).xlsx')
+df = pd.read_csv('subproblem.csv')
 
-data = pd.concat([data1, data2], ignore_index=True)
-data.to_csv('columbia_full.csv', index=False)
 
-# Opening demand file
-df = pd.read_csv('columbia_full.csv')
-df.rename(columns={'FechaPlanInicioTransporte':'date', 'RegionalDistribucion': 'region','IDCentro':'Center_ID','CodigoCliente':'Client_ID'}, inplace=True)
-df.drop(columns = ['Plan_Vol','plan_boxes','DocumentoTransporte','Ordered_vol'], inplace=True)
-
-# Merging with the coordinates of the centers
-coordinates_centers = pd.read_csv('Coordenadas CDs.csv', encoding='latin1')
-coordinates_centers.drop(columns=['Nombre','Direccin','CD.1'], inplace=True)
-coordinates_centers.rename(columns={'CD':'CD','Latitud':'Latitude_CD','Longitud':'Longitude_CD'}, inplace=True)
-
-df = df.merge(coordinates_centers, how='inner', left_on='Center_ID', right_on='CD')
-
-df.drop(columns=['CD'], inplace=True)
-# Merging with the coordinates of the clients
-coordinates_clients = pd.read_csv('Coordenadas Clientes.csv', encoding='latin1')
-coordinates_clients = coordinates_clients[['CD CODE','CLIENT #','LATITUDE','LENGTH']]
-coordinates_clients.rename(columns={'CD CODE':'Center_ID','CLIENT #':'Client_ID','LATITUDE':'Latitude_Client','LENGTH':'Longitude_Client'}, inplace=True)
-coordinates_clients
-
-df = df.merge(coordinates_clients, how='inner', left_on=['Client_ID','Center_ID'], right_on=['Client_ID','Center_ID'])
-
-# Translating boxes into hL
-df['ordered_volume'] = df['ordered_boxes'] * 0.0756
-
-# Merge test
